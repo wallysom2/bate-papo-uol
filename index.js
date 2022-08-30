7a15f839367af673e6c01e2d26ce804b2b529153
Wallyson <wallysom2@gmail.com>
2022-08-30 09:27:55 -0300

    construindo rota participants

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ import dotenv from "dotenv";
 const app = express();
 app.use(cors());
 app.use(json());
+dotenv.config ();
+
+let db = null;
+const MongoClient = new MongoClient (process.env.MONGO_URI);
+
+app.post ("/participants", async (req,res) => {
+    const newUSer = req.body;
+    const participantsSchema = joi.object ({
+        name: joi.string().required()
+    })
+    
+})
 
 
 app.listen(5000, () => console.log(
