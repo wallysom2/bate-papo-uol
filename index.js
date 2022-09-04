e4b7158ff7de7abeecb92227903ef08495353f3b
Matheus <wallysom2@gmail.com>
2022-09-03 21:59:53 -0300

    rota get participants

--- index.js
+++ index.js
@@ -11,16 +11,51 @@ app.use(cors());
 app.use(json());
 dotenv.config ();
 
-let db = null;
-const MongoClient = new MongoClient (process.env.MONGO_URI);
+let db;
+const mongoClient = new MongoClient (process.env.MONGO_URI);
 
 app.post ("/participants", async (req,res) => {
     const newUSer = req.body;
     const participantsSchema = joi.object ({
         name: joi.string().required()
-    })
+    });
+
+    const validacao = participantsSchema.validate(newUSer);
+    if(validacao.error) {
+      res.status(422).send(validacao.error.details);
+      return;
+    }
+
+    try {
+        await mongoClient.connect();
+        db = mongoClient.db("uol-db");
     
+        await db.collection("users").insertOne({...newUSer});
+        res.sendStatus(201);
+    
+        mongoClient.close();
+      } catch (e) {
+        res.status(500).send("Ocorreu um erro ao criar novo usario!", e);
+        mongoClient.close();
+      }
 })
+
+app.get("/participants", async (req, res) => {
+
+  try {
+    await mongoClient.connect();
+    db = mongoClient.db("uol-db");
+
+    const usarios = await db.collection("users").find().toArray();
+    res.send(usarios);
+
+    mongoClient.close();
+  } catch (e) {
+    res.status(500).send("Ocorreu um erro ao obter os usuários!", e);
+    mongoClient.close();
+  }
+});
+
 
 
 app.listen(5000, () => console.log(
