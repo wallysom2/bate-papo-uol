dc888f0332845f9e7aadd400733abb3773fb9baf
Matheus <wallysom2@gmail.com>
2022-09-05 17:19:17 -0300

    fazendo ajustes na rota de mensagens

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import joi from 'joi'
 import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
 import dayjs from "dayjs";
-import { stripHtml } from "string-strip-html";
 
 const app = express();
 app.use(cors());
@@ -14,55 +13,227 @@ app.use(json());
 dotenv.config ();
 
 let db;
-const mongoClient = new MongoClient (process.env.MONGO_URI);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 const promise = mongoClient.connect();
 
+promise.then(() => {
+  db = mongoClient.db("uol-db");
+})
+promise.catch(res => console.log(chalk.red("Erro ao conectar a base"), res))
 
-app.post ("/participants", async (req,res) => {
-    const newUSer = req.body;
-    const participantsSchema = joi.object ({
-        name: joi.string().required().unique()
+app.post("/participants", async (req, res) => {
+  const { name } = req.body;
+
+
+  const schemaNameValidate = joi.object({
+    name: joi.string().alphanum().min(1).required(),
+  });
+
+  const validateName = schemaNameValidate.validate({ name });
+
+  if (validateName.error) {
+    res.sendStatus(422);
+    return;
+  }
+
+  try {
+
+    const user = await db.collection("participants").findOne({ name });
+    db.collection("participants").find().toArray().then(users => {
+      console.log(users);
     });
+    console.log(user)
 
-    const validacao = participantsSchema.validate(newUSer);
-    if(validacao.error) {
-      res.status(422).send(validacao.error.details);
+    if (user) {
+      res.sendStatus(409);
+      return;
+
+    } else {
+      await db.collection("participants").insertOne({ name, lastStatus: Date.now() });
+      db.collection("participants").find().toArray().then(users => {
+        console.log(users);
+      });
+
+      await db.collection("messages").insertOne({
+        from: name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs().format("HH:mm:ss"),
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    mongoClient.close()
+  }
+  res.sendStatus(201);
+  return;
+
+});
+
+app.get("/participants", async (req, res) => {
+  try {
+    const TodosParticipantes = db.collection("participants").find().toArray()
+    res.send(TodosParticipantes);
+    mongoClient.close();
+  } catch (error) {
+    res.sendStatus(404);
+    mongoClient.close();
+  }
+
+})
+app.post("messages", async (req, res) => {
+  const { to, text, type } = req.body
+  const participant = req.header.user;
+
+  const schemaMessage = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().allow('message', 'private_message'),
+  })
+
+  const validateMessage = schemaMessage.validate({ to, type, text }, { abortEarly: false });
+  if (!validateMessage) {
+     res.send(422);
+  } else {
+    res.sendStatus(422).send("xabu", error)
+  }
+
+  try {
+    const user = db.collection("participants").findOne({ participant })
+    if (!user) {
+      res.sendStatus(422);
       return;
     }
 
-    try {
-        await mongoClient.connect();
-        db = mongoClient.db("uol-db");
-    
-        await db.collection("users").insertOne({...newUSer});
-        res.sendStatus(201);
-    
-        mongoClient.close();
-      } catch (e) {
-        res.status(500).send("Ocorreu um erro ao criar novo usario!", e);
-        mongoClient.close();
-      }
+    await db.collection("messages").insertOne({
+      from: user.name,
+      to,
+      text,
+      type,
+      time: dayjs().format("HH:mm:ss"),
+    });
+    res.sendStatus(201);
+    return;
+
+  } catch (e) {
+    console.log(e)
+  }
 })
 
-app.get("/participants", async (req, res) => {
-
+app.get("messages", async (req, res) => {
+  const user = req.headers.user
+  const limite = parseInt(req.query.limit)
+  let arrMensagens = []
   try {
-    await mongoClient.connect();
-    db = mongoClient.db("uol-db");
+    const messages = await db.collection("messages").find().toArray()
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i].type === "private_message" && (messages[i].to === user || messages[i].from === user)) {
+        arrMensagens.push(messages[i]);
+      }
+      if (messages[i].type === "message" || messages[i].type === "status") {
+        arrMensagens.push(messages[i]);
+      }
+    }
+    if (!limit) {
+      return res.send(arrMensagens)
+    }
 
-    const usarios = await db.collection("users").find().toArray();
-    res.send(usarios);
+    let reverseMessages = [...arrMensagens].reverse()
+    let limitMessages = reverseMessages.splice(0, limit)
+    res.send(limitMessages)
 
-    mongoClient.close();
   } catch (e) {
-    res.status(500).send("Ocorreu um erro ao obter os usuários!", e);
+    console.log(e)
+  }
+
+})
+
+app.post ("/status", async (req, res) => {
+  const user = req.headers.user
+  try {
+    const findUser = await db.collection("participants").findOne({ user });
+    if (!findUser) {
+      res.sendStatus(404)
+      return
+    }
+    await db.collection("participants").updateOne({ name: user },
+      { $set: { lastStatus: Date.now() } });
+    res.sendStatus(200);
+    return;
+  } catch (e) {
+    res.send(422).send("nao foi possivel concluir solicitacao", e);
     mongoClient.close();
   }
-});
+
+
+  app.delete("/messages/:id", async (req, res) => {
+    const user = req.headers.user
+    const { id } = req.params
+    try {
+      const messageId = await db.collection("messages").findOne({ _id: new ObjectId(id) })
+      if (!messageId) {
+        res.sendStatus(404)
+        return
+      }
+      if (user !== messageId.from) {
+        res.sendStatus(401);
+        return;
+      }
+      await db.collection("messages").deleteOne({ _id: new ObjectId(id) });
+    } catch (e) { }
+
+  })
+
+  app.put("/messages/:id", async (req, res) => {
+    const { to, type, text } = req.body
+    const user = req.headers.user
+    const { id } = req.params
+
+    const schemaMessage = joi.object({
+      to: joi.string().required(),
+      text: joi.string().required(),
+      type: joi.string().allow('message', 'private_message'),
+    })
+    const validateMessage = schemaMessage.validate({ to, type, text }, { abortEarly: false });
+    if (validateMessage.error) {
+      res.sendStatus(422);
+      return;
+    }
+    try {
+      const findUser = await db.collection("participants").findOne({ name: user })
+      if (!findUser) {
+        res.sendStatus(422);
+        return;
+      }
+      const messageId = await db.collection("messages").findOne({ _id: new ObjectId(id) })
+      if (user !== messageId.from) {
+        res.sendStatus(401);
+        return;
+      }
+      if (!messageId) {
+        res.sendStatus(404)
+        return;
+      }
+      await db.collection("messages").updateOne(
+        { _id: messageId._id, },
+        {
+          $set: {
+            from,
+            to,
+            type,
+            text,
+            time: dayjs().format("HH:mm:ss")
+          }
+        }
+      )
+
+    } catch (e) { console.log(e) }
+  })
+})
 
 
 
-app.listen(5000, () => console.log(
-    chalk.blue.bold(
-        "Server is running on port 5000"
-)))
+app.listen(5000, () => {
+  console.log("rodando na porta 5000")
+})
